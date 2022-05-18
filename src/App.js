646ec782099f67a2e6041ded7040a6a0dcdeb9bb
Anisat Akinbani <anisatakinbani13@gmail.com>
2022-05-18 20:46:54 +0100

    Code clean up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import AddTodo from "./components/AddTodo";
+import Todos from './components/Todos'
 
-function App() {
+const App = () => {
+  const initialTodos = JSON.parse(localStorage.getItem('todos')) || [];
+  const [todos, setTodos] = useState(initialTodos)
+  const [submitted, setSubmitted] = useState(false)
+
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos]);
+
+  const markTodo = (id) => {
+    const newTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+
+  const onAddTodo = (title) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTodo = { id, title, isComplete: false };
+    setTodos([...todos, newTodo]);
+
+    setSubmitted(true)
+  };
+
+  const onDeleteTodo = id => {
+    const filterTodo = todos.filter((todo) => todo.id !== id)
+    setTodos(filterTodo)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AddTodo onAddTodo={onAddTodo} submitted={submitted} setSubmitted = {setSubmitted} />
+      <Todos todos={todos} markTodo={markTodo} onDeleteTodo={onDeleteTodo} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
