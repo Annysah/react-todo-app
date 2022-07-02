7ef225f450fcc6f2b196399896d726e95f25d73b
Anisat Akinbani <anisatakinbani13@gmail.com>
2022-07-02 19:15:43 +0100

    Update todo code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from "react";
 import AddTodo from "./components/AddTodo";
-import Todos from './components/Todos'
+import Todos from "./components/Todos";
 
 const App = () => {
-  const [todos, setTodos] = useState([])
-  const [submitted, setSubmitted] = useState(false)
+  const initialTodos = JSON.parse(localStorage.getItem("todos")) || [];
+  const [todos, setTodos] = useState(initialTodos);
+  const [submitted, setSubmitted] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   const markTodo = (id) => {
-    const newTodos = todos.map(todo => {
+    const newTodos = todos.map((todo) => {
       if (todo.id === id) {
         todo.isComplete = !todo.isComplete;
       }
@@ -16,26 +21,29 @@ const App = () => {
     setTodos(newTodos);
   };
 
-
   const onAddTodo = (title) => {
-    const id = Math.floor(Math.random() * 10000) + 1
+    const id = Math.floor(Math.random() * 10000) + 1;
     const newTodo = { id, title, isComplete: false };
     setTodos([...todos, newTodo]);
 
-    setSubmitted(true)
+    setSubmitted(true);
   };
 
-  const onDeleteTodo = id => {
-    const filterTodo = todos.filter((todo) => todo.id !== id)
-    setTodos(filterTodo)
-  }
+  const onDeleteTodo = (id) => {
+    const filterTodo = todos.filter((todo) => todo.id !== id);
+    setTodos(filterTodo);
+  };
 
   return (
     <div>
-      <AddTodo onAddTodo={onAddTodo} submitted={submitted} setSubmitted = {setSubmitted} />
+      <AddTodo
+        onAddTodo={onAddTodo}
+        submitted={submitted}
+        setSubmitted={setSubmitted}
+      />
       <Todos todos={todos} markTodo={markTodo} onDeleteTodo={onDeleteTodo} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
